c4d800cef9811a915fa1b3a86bae70f55c970ee8
igoribon <igoribon@gmail.com>
2025-05-23 11:52:13 +0200

    Por fin traducido

--- src/main.js
+++ src/main.js
@@ -2,14 +2,13 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
-
+import { i18n } from './i18n/i18n'
 import App from './App.vue'
 import router from './router'
-import { createI18n } from 'vue-i18n'
+
 
 const app = createApp(App)
-
+app.use(i18n)
 app.use(createPinia())
 app.use(router)
 
