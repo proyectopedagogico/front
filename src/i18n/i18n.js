0f0836e025a1f8570840029e5317c41e2c3a34a0
igoribon <igoribon@gmail.com>
2025-05-28 11:40:22 +0200

    incluido euskera y areglado el js del plugin para que traduzca las páginas

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,6 +1,9 @@
 import { createI18n } from 'vue-i18n'
 import es from '@/locales/es.json'
 import en from '@/locales/en.json'
+import eu from '@/locales/eu.json'
+import ar from '@/locales/ar.json'
+import ur from '@/locales/ur.json'
 
 export const i18n = createI18n({
   legacy: false, // Usa la API de Composition
@@ -8,6 +11,9 @@ export const i18n = createI18n({
   fallbackLocale: 'es',
   messages: {
     es,
-    en
+    en,
+    eu,
+    ar,
+    ur
   }
 })
